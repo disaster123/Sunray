27b74fd504dff40b7ee3494682aa154b263c8755
Stefan Priebe <stefan-git@prie.be>
2022-11-12 13:15:56 +0100

    comm/LineTracker: print more motor values

--- sunray/comm.cpp
+++ sunray/comm.cpp
@@ -1299,6 +1299,10 @@ void outputConsole(){
     CONSOLE.print(motor.motorMowPWMCurr);
     CONSOLE.print(" motorMowRpmCurrLP=");
     CONSOLE.print(motor.motorMowRpmCurrLP);
+    CONSOLE.print(" motorMowSense=");
+    CONSOLE.print(motor.motorMowSense);
+    CONSOLE.print(" motorMowSenseLP=");
+    CONSOLE.print(motor.motorMowSenseLP);
     CONSOLE.println();
     //logCPUHealth();    
   }
