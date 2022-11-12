b099fe7e6b7ed706903e3793574b2b6d124e7fe5
Stefan Priebe <stefan-git@prie.be>
2022-11-12 12:11:47 +0100

    map: findPath: reset timeouts inside loop

--- sunray/map.cpp
+++ sunray/map.cpp
@@ -2116,6 +2116,9 @@ bool Map::findPath(Point &src, Point &dst){
         nextProgressTime = millis() + 4000;          
         // CONSOLE.print(".");
         watchdogReset();     
+        resetImuTimeout();
+        resetOverallMotionTimeout();
+        updateGPSMotionCheckTime();
       }
       timeout--;            
       if (timeout == 0){
