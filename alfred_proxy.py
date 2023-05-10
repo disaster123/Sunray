687b6b26ada5ef052ce403b6401fb39b16ad2e67
Stefan Priebe <stefan-git@prie.be>
2023-05-10 21:54:05 +0200

    alfred_proxy: use different path for POST

--- alfred_proxy.py
+++ alfred_proxy.py
@@ -8,12 +8,15 @@ import re
 import gzip
 
 new_host = "grauonline.de"
-target_uri = "/alexwww/ardumower/ardumower-sunray-app/"
+target_uri_default = "/alexwww/ardumower/ardumower-sunray-app/"
 
 async def handle_request(request):
     async with aiohttp.ClientSession(auto_decompress=False) as session:
+        target_uri = target_uri_default
+        if request.method == "POST":
+            target_uri = "/"
         target_url = request.url.with_scheme("http").with_host(new_host).with_port(80).with_path(target_uri + request.match_info['tail'])
-        # print("fetch target: " + str(target_url))
+        print("fetch target: " + str(target_url))
         async with session.request(request.method, target_url, headers=request.headers, data=await request.read()) as response:
             response_headers = response.headers.copy()
             # Fge CORS-Header hinzu, um Cross-Origin-Anfragen zu ermglichen
