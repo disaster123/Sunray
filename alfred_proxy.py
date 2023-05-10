e8751e1bcf676c87f34c2a1c7ab0e9e5c507c6bb
Stefan Priebe <stefan-git@prie.be>
2023-05-10 21:34:32 +0200

    alfred_proxy: insert custom meta tags for ios

--- alfred_proxy.py
+++ alfred_proxy.py
@@ -4,6 +4,9 @@
 import aiohttp
 from aiohttp import web
 
+import re
+import gzip
+
 new_host = "grauonline.de"
 target_uri = "/alexwww/ardumower/ardumower-sunray-app/"
 
@@ -16,8 +19,28 @@ async def handle_request(request):
             # Fge CORS-Header hinzu, um Cross-Origin-Anfragen zu ermglichen
             response_headers["Access-Control-Allow-Origin"] = "*"
 
+            content = await response.read()
+
+            if request.url.path == '/':
+                if response.headers.get('Content-Encoding') == 'gzip':
+                    content = gzip.decompress(content)
+                    response_headers["Content-Encoding"] = ""
+
+                head_start = re.search(r"<head>", content.decode(), re.IGNORECASE)
+
+                if head_start:
+                    # Text, der nach dem <head>-Tag eingefügt werden soll
+                    meta_tag = "\n<meta name=apple-mobile-web-app-capable content=yes>\n<meta name=apple-mobile-web-app-status-bar-style content=default>\n<meta name=mobile-web-app-capable content=yes>\n"
+            
+                    # Position des <head>-Tags im HTML-Code ermitteln
+                    head_start_pos = head_start.start()
+                    head_end_pos = head_start.end()
+            
+                    # Neuen HTML-Code erstellen, mit dem eingefügten Meta-Tag
+                    content = content[:head_end_pos] + meta_tag.encode() + content[head_end_pos:]
+
             # Erstelle eine HTTP-Antwort an den Client
-            return web.Response(status=response.status, headers=response_headers, body=await response.read())
+            return web.Response(status=response.status, headers=response_headers, body=content)
 
 app = web.Application()
 app.router.add_route('*', '/{tail:.*}', handle_request)
