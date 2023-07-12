e2439cd43b55cea02ae727cfe52dda7ebe5f62e2
Stefan Priebe <stefan-git@prie.be>
2023-07-12 11:00:25 +0200

    angetotargetfits: lower check to 10 degrees instead of 20

--- sunray/LineTracker.cpp
+++ sunray/LineTracker.cpp
@@ -158,7 +158,7 @@ void trackLine(bool runControl){
     if (SMOOTH_CURVES)
       angleToTargetFits = (fabs(trackerDiffDelta)/PI*180.0 < 120);
     else     
-      angleToTargetFits = (fabs(trackerDiffDelta)/PI*180.0 < 20);
+      angleToTargetFits = (fabs(trackerDiffDelta)/PI*180.0 < 10);
   } else {
      angleToTargetFits = true;
   }
