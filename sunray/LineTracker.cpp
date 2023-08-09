74872228562523b52670d1eb52e17bf8cbb6a167
Stefan Priebe <stefan-git@prie.be>
2023-08-09 20:58:42 +0200

    LineTracker: use get_turn_direction_preference for trackReverse

--- sunray/LineTracker.cpp
+++ sunray/LineTracker.cpp
@@ -191,11 +191,7 @@ void trackLine(bool runControl){
 
     angular = angularspeed / 180.0 * PI; //  40,6 degree/s (0.7 rad/s);              
     if ((!rotateLeft) && (!rotateRight)){ // decide for one rotation direction (and keep it)
-      int r = 0;
-      // no idea but don't work in reverse mode...
-      if (!maps.trackReverse) {
-        r = get_turn_direction_preference();
-      }
+      int r = get_turn_direction_preference();
       // store last_rotation_target point
       last_rotation_target.setXY(target.x(), target.y());
 
